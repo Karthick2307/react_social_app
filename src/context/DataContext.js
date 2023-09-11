9ed3056d35d79c2123b4465780bb5405b23e37db
unknown <Ayyappan>
2023-09-12 00:48:30 +0530

    first commit

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,26 +1,16 @@
-import { Link, Routes, Route, useNavigate} from "react-router-dom";
-import About from "./About";
-import Footer from "./Footer";
-import Header from "./Header";
-import Home from "./Home";
-import Missing from "./Missing";
-import Nav from "./Nav";
-import NewPost from "./NewPost";
-import PostPage from "./PostPage";
-import Post from "./Post";
-import { useEffect, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import Post from "../Post";
 import { format } from "date-fns";
-import api from "./api/posts";
-import EditPost from "./EditPost";
-import useWindowSize from "./hooks/useWindowSize";
-import useAxiosFetch from "./hooks/useAxiosFetch";
-import posts from "./api/posts";
+import api from "../api/posts"
+import EditPost from "../EditPost";
+import useWindowSize from "../hooks/useWindowSize";
+import useAxiosFetch from "../hooks/useAxiosFetch";
+import { useNavigate } from "react-router-dom";
 
+const DataContext = createContext()
 
-
-
-function App() {
-  const [posts, setPosts] = useState([]);
+export const DataProvider = ({childern}) => {
+    const [posts, setPosts] = useState([]);
     const [search, setSearch]= useState('');
     const [searchResults, setSearchResults] = useState([]);
     const [postTitle, setPostTitle] = useState('');
@@ -85,49 +75,20 @@ function App() {
       console.log(`Error: ${err.message}`);
      } 
   }
-  
+    return (
+        <DataContext.Provider value={{
+           width, search, setSearch, 
+           searchResults, fetchError, isLoading,
+           handleSubmit, postTitle, setPostTitle, postBody, setPostBody,
+           posts, handleEdit, editBody, setEditBody, editTitle, setEditTitle, 
+           handleDelete
+           
 
-  return (
-    <div className="App">
-        <Header title="Karthick Social Media" width={width} />
-        <Nav 
-          search={search}
-          setSearch={setSearch}
-        />
-        <Routes>
-          <Route path="/" element = {
-          <Home
-            posts={searchResults}
-            fetchError={fetchError}
-            isLoading={isLoading}
-           />} />
-          <Route path="post">
-            <Route index element = {<NewPost
-              handleSubmit={handleSubmit}
-              postTitle={postTitle}
-              setPostTitle={setPostTitle}
-              postBody={postBody}
-              setPostBody={setPostBody}
-            />} />
-          <Route path=":id" element = {
-            <PostPage 
-              posts={posts} 
-              handleDelete={handleDelete} />} />
-          </Route>
-          <Route path="/edit/:id" element = {<EditPost 
-          posts={posts}
-            handleEdit={handleEdit}
-            editBody={editBody}
-            setEditBody={setEditBody}
-            editTitle={editTitle}
-            setEditTitle={setEditTitle}
-          />} />
-          <Route path="about" element = {<About />} />
-          <Route path="*" element = {<Missing />} />
-        </Routes>
-        <Footer/>
-    </div>
-  );
+        }}>
+            {childern}
+        </DataContext.Provider>
+    )
 }
 
-export default App;
+
+export default DataContext
